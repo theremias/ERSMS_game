9f7819e2ce76ee97a6f7c69d8d3f981a0f2ed099
theremias <104557989+theremias@users.noreply.github.com>
2023-11-23 11:40:41 +0100

    Update common.h
    added struct 'route' and 'location' and struct 'Places'

--- game/src/common.h
+++ game/src/common.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include "math.h"
+
+
+typedef struct route {
+      int no_lengths;
+      int grade;
+} route_t;
+
+
+typedef struct location {
+      int NEVIM;
+} location_t;
+
+enum Places {Aljazev dom, Triglav, Blllaaa}
